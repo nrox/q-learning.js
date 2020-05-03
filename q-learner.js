0c5ea9592d712867bda2da37c93c4f7d0c3dae7f
Nuno Rocha <nrox@>
2020-05-03 15:25:17 +0200

    use temporary file q-learner.js for refactoring

--- q-learner.js
+++ q-learner.js
@@ -1,27 +1,177 @@
 "use strict";
 
+const generateRandom = new Math.seedrandom()
+
 class Action {
-    constructor(nextState, reward, actionName){
+    constructor(nextState, reward, name){
         this.nextState = nextState;
         this.reward = reward;
-        this.actionName = actionName;
+        this.name = name;
     }
 }
+
 class State {
     constructor(name){
         this.name = name
         this.actions = {}
         this.actionsList = []
     }
-    addAction(nextState, reward, actionName){
-        let action = new Action(nextState, reward, actionName)
-        this.actions[actionName] = action
+    addAction(nextState, reward, name){
+        let action = new Action(nextState, reward, name)
+        this.actions[name] = action
         this.actionsList.push(action)
         return action
     }
     randomAction = () => {
-        let index = Math.floor(this.actionsList.length * Math.random())
+        let index = Math.floor(this.actionsList.length * generateRandom())
         return this.actionsList[index]
     }
+}
 
+class QLearner {
+
+    /**
+     * 
+     * @param {number} gamma The learning rate 
+     */
+    constructor(gamma){
+        this.gamma = gamma || 0.8
+        this.rewards = {}
+        this.states = {}
+        this.statesList = []
+        this.currentState = null
+    }
+
+    /**
+     * Add an action from (state) to (state), with a predefined reward.
+     */
+    add(from, to, reward, actionName){
+        if (!this.states[from]) this.addState(from)
+        if (!this.states[to]) this.addState(to)
+        this.setReward(from, actionName, reward)
+        return this.states[from].addAction(to, reward, actionName)
+    }
+    addState(name){
+        let state = new State(name);
+        this.states[name] = state;
+        this.statesList.push(state);
+        return state;
+    }
+    setState(name){
+        this.currentState = this.states[name] || new State(name)
+        return this.currentState
+    }
+    getState(){
+        return this.currentState && this.currentState.name
+    }
+    randomState(){
+        let index = Math.floor(this.statesList.length * generateRandom())
+        return this.statesList[index]
+    }
+
+    /**
+     * The maximum reward achievable from the state, by looking at the rewards table.
+     */
+    optimalFutureValue(state){
+        let stateRewards = this.rewards[state]
+        let max = 0
+        for (let action in stateRewards){
+            if (stateRewards.hasOwnProperty(action)){
+                max = Math.max(max, stateRewards[action] || 0)
+            }
+        }
+        return max;
+    }
+
+    /**
+     * If there are defined rewards for the action, from the state.
+     */
+    knowsAction(state, action){
+        return (this.rewards[state] || {}).hasOwnProperty(action)
+    }
+
+    /**
+     * From current state, apply the action (name), and set the action's next state as current state.
+     */
+    applyAction(name){
+        let actionObject = this.states[this.currentState.name].actions[name];
+        if (actionObject){
+            this.currentState = this.states[actionObject.nextState];
+        }
+        return actionObject && this.currentState;
+    }
+
+    /**
+     * Get the best action from the state (name), which is the one with immediate best reward.
+     * During the selection process, if states have the same reward, choose one with probability 50%.
+     */
+    bestAction(state){
+        let stateRewards = this.rewards[state] || {}
+        let bestAction = null;
+        for (let action in stateRewards){
+            if (stateRewards.hasOwnProperty(action)){
+                if (!bestAction){
+                    bestAction = action
+                } else if ((stateRewards[action] == stateRewards[bestAction]) && (generateRandom()>0.5)){
+                    bestAction = action
+                } else if (stateRewards[action] > stateRewards[bestAction]){
+                    bestAction = action
+                }
+            }
+        }
+        return bestAction
+    }
+
+    /**
+     * From current state, choose the best action and apply it, 
+     * doing a transition for the next state and setting this as currentState
+     */
+    runOnce(){
+        let bestActionName = this.bestAction(this.currentState.name);
+        if (!bestActionName) return undefined
+        let action = this.states[this.currentState.name].actions[bestActionName];
+        if (action){
+            this.currentState = this.states[action.nextState];
+        }
+        return action && this.currentState;
+    }
+
+    /**
+     * Set a reward for performing the action from a state
+     */
+    setReward(stateName, actionName, reward){
+        if(!this.rewards[stateName]) this.rewards[stateName] = {}
+        this.rewards[stateName][actionName] = reward
+    }
+
+    /**
+     * Do a single learning step from current state.
+     */
+    step(){
+        //set current state randomly if not defined
+        if(!this.currentState) this.currentState = this.randomState()
+        if (!this.currentState) return null;        
+        let state = this.currentState
+
+        //get some action from current state
+        let action = state.randomAction();
+        if (!action) return null;
+
+        //rewards is a map {state#: {action#: reward, ...}, ...}
+        let reward = (action.reward || 0) + this.gamma * this.optimalFutureValue(action.nextState)
+        this.setReward(state.name,action.name, reward)
+        this.currentState = this.states[action.nextState]
+        return this.currentState
+    }
+
+    /**
+     * From the current state, apply the learning process #steps
+     */
+    learn(steps){
+        steps = Math.max(1, steps || 0)
+        while (steps--){
+            this.currentState = this.randomState()
+            this.step()
+        }
+    }
 }
--- tests/unit-tests.js
+++ tests/unit-tests.js
@@ -2,9 +2,17 @@
 
 describe("q-learner.js", () => {
 
+    it ("random", ()=>{
+        for (let i = 0; i < 100; i++){
+            let number = generateRandom()
+            expect(number >=0).toBe(true)
+            expect(number < 1).toBe(true)
+        }
+    })
+
     it("Action", () => {
       let action = new Action("next", 1.2, "action")
-      expect(action.actionName).toBe('action')
+      expect(action.name).toBe('action')
       expect(action.reward).toBe(1.2)
       expect(action.nextState).toBe("next")
     })
@@ -18,7 +26,7 @@ describe("q-learner.js", () => {
         let a2 = state.addAction("some", 1.0, "a2")
         let countA1 = 0
         let countA2 = 0
-        let maxCount = 10000
+        let maxCount = 1000
         for (let i = 0; i < maxCount; i++){
             switch(state.randomAction()){
                 case a1: countA1++; break;
@@ -29,8 +37,73 @@ describe("q-learner.js", () => {
         expect(countA1 + countA2).toBe(maxCount)
         expect(countA1 > 0).toBe(true, `There is 1/${maxCount} chances of randomAction() not returning 1 of 2 actions and this test fail.`)
         expect(countA2 > 0).toBe(true, `There is 1/${maxCount} chances of randomAction() not returning 1 of 2 actions and this test fail.`)
-
     })
+
+    it("QLearner.add action", () => {
+        let learner = new QLearner
+        let a1 = learner.add("s0","s1",1.0,"a1")
+        expect(a1.name).toBe("a1")
+        expect(a1.reward).toBe(1.0)
+        expect(a1.nextState).toBe("s1")
+    })
+
+    it("QLearner.{addState, randomState)", () => {
+        let learner = new QLearner
+        expect(learner.randomState()).toBe(undefined)
+        let s0 = learner.addState("s0")
+        expect(learner.randomState()).toBe(s0)
+    })
+
+    it("QLearner.{setState, getState}", () => {
+        let learner = new QLearner
+        let s0 = learner.setState("s0")
+        expect(s0 instanceof State).toBe(true)
+        let s = learner.getState()
+        expect(s).toBe(s0.name)
+    })
+
+    it("QLearner.optimalFutureValue", () => {
+        let learner = new QLearner
+        learner.add("from","to1", 1.0, "a1")
+        learner.add("from","to2", -1.0, "a2")
+        learner.add("from","to3", 2.0, "a3")
+        let value = learner.optimalFutureValue("from")
+        expect(value).toBe(2.0)
+    })
+
+    it("QLearner.runOnce", () => {
+        let learner = new QLearner
+        learner.setState("some")
+        expect(learner.runOnce()).toBe(undefined)
+        learner.add("from","to", 1.0, "a")
+        learner.setState("from")
+        let to = learner.runOnce();
+        expect(to).not.toBe(undefined)
+        expect(to.name).toBe("to")
+    })
+
+    it("QLearner.bestAction", () => {
+        let learner = new QLearner
+        learner.add("s0", "s1", 1.0, "a1")
+        learner.add("s0", "s2", -1.0, "a2")
+        learner.add("s0", "s3", 2.0, "a3")
+        expect(learner.bestAction("s0")).toBe("a3")
+    })
+
+    it("QLearner.step", () => {
+        let learner = new QLearner
+        learner.add("s0", "s1", 0.0, "a1")
+        learner.add("s1", "s2", 1.0, "a2")
+        learner.setState("s0")
+        let s = learner.step()
+        expect(s.name).toBe("s1")
+        expect(learner.currentState).toBe(s)
+    })
+
+    it("...", () => {
+        expect(true).toBe(true)
+    })
+
 })
       
   
